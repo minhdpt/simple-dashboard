c3123219ab5426aa4bf0961d344e336c1e9f9d83
Dang Pham The Minh <minh.dangphamthe@gameloft.com>
2020-06-15 11:31:43 +0700

    - nav bar with signin/signup (https://www.youtube.com/watch?v=jKqnfzdPqJI&list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3&index=4)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
-
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+      </div>
+    </BrowserRouter>
 
-    </div>
   );
 }
 
