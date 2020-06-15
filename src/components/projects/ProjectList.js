ca48a2baa191cca43d6fbf80b33fb5fe1ec1e1d1
Dang Pham The Minh <minh.dangphamthe@gameloft.com>
2020-06-15 14:35:51 +0700

    - Project summary. (https://www.youtube.com/watch?v=BRQLXTa0I94&list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3&index=6)

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,29 +1,9 @@
 import React from 'react'
-
+import ProjectSummary from '../projects/ProjectSumary'
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">June 15th, 12 AM</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">June 15th, 12 AM</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">June 15th, 12 AM</p>
-                </div>
-            </div>
+            <ProjectSummary/>
         </div>
     )
 }
---
+++ src/components/projects/ProjectSumary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ProjectList from './ProjectList'
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by the Net Ninja</p>
+                <p className="grey-text">Sep 3rd, 2am</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary
